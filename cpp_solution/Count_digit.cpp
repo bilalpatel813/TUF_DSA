f62c8859682401ab190102f9d2c50d23406a3b29
Patel Bilal <bilalpatel78600@gmail.com>
2026-02-14 23:12:11 +0530

    Added cpp question 4

---
+++ cpp_solution/Count_digit.cpp
@@ -0,0 +1,15 @@
+//TUF QUESTION 4
+//You are given an integer n. You need to return the number of digits in the number.
+//The number will have no leading zeroes, except when the number is 0 itself
+//solution:
+class Solution {
+public:
+    int countDigit(int n) {
+        int count =0;
+        while(n!= 0){
+            n = n/10;
+            count++;
+        }
+        return count;
+    }
+};
