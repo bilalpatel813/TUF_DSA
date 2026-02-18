d58d474d49160052bb3bc6fe48c8754cf32141e1
Patel Bilal <bilalpatel78600@gmail.com>
2026-02-19 00:04:45 +0530

    Added GCD.Py

---
+++ Python_solutions/GCD.py
@@ -0,0 +1,16 @@
+#TUF QUESTION 7:
+'''You are given two integers n1 and n2. You need find the Greatest Common Divisor (GCD) of the two given numbers. Return the GCD of the two numbers.
+
+The Greatest Common Divisor (GCD) of two integers is the largest positive integer that divides both of the integers.'''
+# SOLUTION:
+    class Solution:
+    def GCD(self, n1, n2):
+        while n1>0 and n2>0:
+            if n1>n2:
+                n1 = n1 % n2
+            else:
+                n2= n2 % n1
+        if n1==0 :
+            return n2
+        return n1
+
