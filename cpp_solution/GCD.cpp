9c691698aaee3d2ff26c2fc7b7d926703d0716df
Patel Bilal <bilalpatel78600@gmail.com>
2026-02-19 00:06:03 +0530

    add GCD in cpp

---
+++ cpp_solution/GCD.cpp
@@ -0,0 +1,22 @@
+//TUF QUESTION 7:
+//You are given two integers n1 and n2. You need find the Greatest Common Divisor (GCD) of the two given numbers. Return the GCD of the two numbers.
+
+//The Greatest Common Divisor (GCD) of two integers is the largest positive integer that divides both of the integers.
+//SOLUTION:
+class Solution {
+public:
+    int GCD(int n1,int n2) {
+        while(n1>0 && n2>0){
+            if(n1>n2){
+                n1= n1%n2;
+            }
+            else{
+                n2=n2%n1;
+            }
+        }
+        if(n1==0){
+            return n2;
+        }
+        return n1;            
+    }
+};
